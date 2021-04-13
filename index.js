a3f18a74478e8f6aa93ff2a092009d6c1790d4a1
taerikor <akdud153@gmail.com>
2021-04-14 03:13:42 +0900

    fixed css

--- index.js
+++ index.js
@@ -1,52 +1,38 @@
-const range = document.querySelector('.range')
-const per = document.querySelector('.per_js')
+const btnContainer = document.querySelector('.btn_container')
+const button = btnContainer.querySelectorAll('span')
 const source = document.getElementById('first_src')
 const video = document.getElementById('video_js')
+const musicBtn = document.querySelector('.modal_btn')
+const musicModal = document.querySelector('.modal')
+const musicCloseBtn = document.querySelector('.modal_close')
 
-const rangeHandler = (e) => {
-    const value = e.target.value
-    per.innerHTML = `${e.target.value}%`
-    if(value >= 20 && value < 40){
+button.forEach(item => item.addEventListener('click',(e) => {
+    const buttonId = e.target.id
+        if(buttonId === 'second_btn'){
         source.setAttribute('src', 'src/video/bada.mp4');
         video.load();
     }
-    if(value >= 40 && value < 60){
+    if(buttonId === 'third_btn'){
         source.setAttribute('src', 'src/video/island.mp4');
         video.load();
     }
-    if(value >= 60 && value < 80){
+    if(buttonId === 'fourth_btn'){
         source.setAttribute('src', 'src/video/jaangnaan.mp4');
         video.load();
     }
-    if(value >= 80){
+    if(buttonId === 'fifth_btn'){
         source.setAttribute('src', 'src/video/nmsh.mp4');
         video.load();
     }
-    if( value < 20 ){
+    if( buttonId === 'first_btn'){
         source.setAttribute('src', 'src/video/24k.mp4');
         video.load();
     }
-}
+}))
 
-
-range.addEventListener('input',rangeHandler)
-
-
-
-
-
-
-
-// const emptyButton = document.querySelectorAll('.far')
-
-
-// const buttonHandle = (e) => {
-//    if(e.target.className === "far fa-circle"){
-//     e.target.className = "fas fa-circle"
-//    }else if( e.target.className === "fas fa-circle"){
-//     e.target.className = "far fa-circle"
-//    }
-// }
-
-
-// emptyButton.forEach(btn => btn.addEventListener('click',buttonHandle))
+musicBtn.addEventListener('click',() => {
+    musicModal.classList.remove('hidden')
+})
+musicCloseBtn.addEventListener('click',() => {
+    musicModal.classList.add('hidden')
+})
