28b42c83ba512c2445ea95d8bef303acb769bb14
taerikor <akdud153@gmail.com>
2021-07-10 15:08:58 +0900

    image optimization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,16 @@ import gulp from 'gulp'
 import gulpPug from 'gulp-pug'
 import del from 'del'
 import ws from 'gulp-webserver'
-
+import image from 'gulp-image'
 const routes = {
     pug:{
         watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
+    },
+    img:{
+        src: "src/image/*",
+        dest: "build/img"
     }
 }
 
@@ -17,13 +21,19 @@ const pug = () => {
         .pipe(gulp.dest(routes.pug.dest))
 }
 
+const img = () => {
+    return gulp.src(routes.img.src)
+        .pipe(image())
+        .pipe(gulp.dest(routes.img.dest))
+}
+
 const clean = () => del(["build"])
 
 const webserver = () => gulp.src("build").pipe(ws({livereload: true, open: true}))
 
 const watch = () => gulp.watch(routes.pug.watch, pug)
 
-const prepare = gulp.series([clean])
+const prepare = gulp.series([clean,img])
 
 const assets = gulp.series([pug])
 
