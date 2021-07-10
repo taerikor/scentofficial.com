f75a336e3803f4a127d9c64b4703bb39451a18d1
taerikor <akdud153@gmail.com>
2021-07-10 16:23:54 +0900

    gulp sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,11 @@ import gulpPug from 'gulp-pug'
 import del from 'del'
 import ws from 'gulp-webserver'
 import image from 'gulp-image'
+import autoPreFixer from 'gulp-autoprefixer'
+import csso from 'gulp-csso'
+
+const sass = require('gulp-sass')(require('sass'));
+
 const routes = {
     pug:{
         watch: "src/**/*.pug",
@@ -12,6 +17,11 @@ const routes = {
     img:{
         src: "src/image/*",
         dest: "build/img"
+    },
+    sass:{
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css"
     }
 }
 
@@ -26,16 +36,28 @@ const img = () => {
         .pipe(image())
         .pipe(gulp.dest(routes.img.dest))
 }
+const styles = () => {
+    return gulp.src(routes.sass.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoPreFixer({
+            browsers: ["last 2 versions"]
+        }))
+        .pipe(csso())
+        .pipe(gulp.dest(routes.sass.dest))
+}
 
 const clean = () => del(["build"])
 
 const webserver = () => gulp.src("build").pipe(ws({livereload: true, open: true}))
 
-const watch = () => gulp.watch(routes.pug.watch, pug)
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug)
+    gulp.watch(routes.sass.watch, styles)
+}
 
 const prepare = gulp.series([clean,img])
 
-const assets = gulp.series([pug])
+const assets = gulp.series([pug,styles])
 
 const postDev = gulp.parallel([webserver, watch])
 
