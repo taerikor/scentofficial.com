a13fe956cb08a9d143c79ab1812a341189b3437d
taerikor <akdud153@gmail.com>
2021-07-10 14:51:06 +0900

    added gulp-babel, task

---
+++ gulpfile.babel.js
@@ -0,0 +1,32 @@
+import gulp from 'gulp'
+import gulpPug from 'gulp-pug'
+import del from 'del'
+import ws from 'gulp-webserver'
+
+const routes = {
+    pug:{
+        watch: "src/**/*.pug",
+        src: "src/*.pug",
+        dest: "build"
+    }
+}
+
+const pug = () => {
+    return gulp.src(routes.pug.src)
+        .pipe(gulpPug())
+        .pipe(gulp.dest(routes.pug.dest))
+}
+
+const clean = () => del(["build"])
+
+const webserver = () => gulp.src("build").pipe(ws({livereload: true, open: true}))
+
+const watch = () => gulp.watch(routes.pug.watch, pug)
+
+const prepare = gulp.series([clean])
+
+const assets = gulp.series([pug])
+
+const postDev = gulp.parallel([webserver, watch])
+
+export const dev = gulp.series([prepare,assets,postDev])
