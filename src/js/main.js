5df94ac37290245294a0c6a4ea3ad2cd204ecb0e
taerikor <akdud153@gmail.com>
2021-08-01 21:41:37 +0900

    animate mouseover pause

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+const link = [
+"/1",
+"/2",
+"/3",
+"/4",
+"/5"
+]
 
 const numBalls = 5;
 const balls = [];
@@ -6,11 +13,12 @@ const balls = [];
 for (let i = 0; i < numBalls; i++) {
     let ball = document.createElement("a");
     ball.classList.add("ball");
+    ball.href = link[i]
     ball.style.background = colors[Math.floor(Math.random() * colors.length)];
     ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
     ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
     ball.style.transform = `scale(${Math.random()})`;
-    ball.style.width = `2em`;
+    ball.style.width = `${i * 2}em`;
     ball.style.height = ball.style.width;
 
     balls.push(ball);
@@ -37,4 +45,8 @@ balls.forEach((el, i, ra) => {
             easing: "ease-in-out"
         }
     );
+
+    el.addEventListener("mouseover",(event)=>{
+        anim.pause()
+    })
 });
