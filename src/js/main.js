ab2ca63f800e0e9470662d9add21a61ad7104dd0
taerikor <akdud153@gmail.com>
2021-08-02 01:21:43 +0900

    changed CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+const colors = ["#d1ce21"];
 const link = [
 "/1",
 "/2",
@@ -6,23 +6,38 @@ const link = [
 "/4",
 "/5"
 ]
+const text = [
+    "ONCE YOU ARE GIVEN CAN NOTHING BE CHANGED",
+    "GOT ME FEELIN' LIKE",
+    "NUMB",
+    "EXIT",
+    "POLYMORPH"
+]
 
 const numBalls = 5;
 const balls = [];
 
 for (let i = 0; i < numBalls; i++) {
     let ball = document.createElement("a");
+    let container = document.createElement("div")
+    let name = document.createElement('span')
+    name.classList.add("name")
+    name.classList.add("hidden")
+    container.classList.add('container')
+    container.append(name)
+    container.append(ball)
     ball.classList.add("ball");
+    name.innerText = text[i]
     ball.href = link[i]
     ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-    ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-    ball.style.transform = `scale(${Math.random()})`;
-    ball.style.width = `${i * 2}em`;
+    container.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    container.style.top = `${Math.floor(Math.random() * 100)}vh`;
+    container.style.transform = `scale(${Math.random()})`;
+    ball.style.width = `2em`;
     ball.style.height = ball.style.width;
 
-    balls.push(ball);
-    document.body.append(ball);
+    balls.push(container);
+    document.body.append(container);
 }
 
 // Keyframes
@@ -46,7 +61,16 @@ balls.forEach((el, i, ra) => {
         }
     );
 
-    el.addEventListener("mouseover",(event)=>{
+    el.addEventListener("mouseover",()=>{
+        console.log(el.childNodes[0])
         anim.pause()
+        document.querySelector(".overlay").classList.remove("hidden")
+        el.childNodes[0].classList.remove("hidden")
+    })
+
+    el.addEventListener("mouseout",() => {
+        anim.play()
+        document.querySelector(".overlay").classList.add("hidden")
+        el.childNodes[0].classList.add("hidden")
     })
 });
